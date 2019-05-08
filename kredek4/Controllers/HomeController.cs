a89a1753f0184e1a922ccb5f92670882f53f742a
Student 249092 <249092@student.pwr.edu.pl>
2019-05-08 19:51:42 +0200

    save commit 1

--- kredek4/Controllers/HomeController.cs
+++ kredek4/Controllers/HomeController.cs
@@ -10,12 +10,38 @@ namespace kredek4.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Lista samochodow do wyswietlenia
+        /// </summary>
+        List<CarViewModel> allCars;
+
+        public HomeController()
+        {
+            allCars = new List<CarViewModel>();
+            allCars.Add(new CarViewModel("Focus", "Ford", 70000, "~/focus.png"));
+            allCars.Add(new CarViewModel("Golf", "VW", 70000, "~/golf.png"));
+            allCars.Add(new CarViewModel("Civic", "Honda", 70000, "~/civic.png"));
+            allCars.Add(new CarViewModel("Megane", "Renault", 70000, "~/megane.png"));
+
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        
+        [HttpGet]
+        public IActionResult InterestingLinks()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCars()
+        {
+            return View(allCars);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ kredek4/Models/CarViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kredek4.Models
+{
+    public class CarViewModel
+    {
+        /// <summary>
+        /// model
+        /// </summary>
+        public string Model { get; set; }
+        
+        /// <summary>
+        /// marka
+        /// </summary>
+        public string Manufacturer { get; set; }
+        
+        /// <summary>
+        /// Cena
+        /// </summary>
+        public decimal Price { get; set; }
+        
+        /// <summary>
+        /// zdjecie
+        /// </summary>
+        public string Photo { get; set; }
+
+        public CarViewModel(String model, string manufacturer, decimal price, string photo)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+            Photo = photo;
+        }
+
+    }
+}
