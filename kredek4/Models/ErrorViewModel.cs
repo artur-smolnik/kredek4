ec15ee8cded797ab9a04f02a8f4466225ac76763
Student 249092 <249092@student.pwr.edu.pl>
2019-05-08 19:30:41 +0200

    Add project files.

---
+++ kredek4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kredek4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
